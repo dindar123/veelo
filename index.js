4fd306114b11d152f6fa0f0997fc9faf45107994
dindar123 <dr.deenmohd@gmail.com>
2017-02-19 11:49:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,16 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://veelo.herokuapp.com/',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://veelo.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Messaging", "_user"] // List of classes to support for query subscriptions
   }
-  
+  push: android: {
+                      senderId: '842306250514',
+                      apikey: 'AIzaSyCl-niHcuWkI0dWgcYEx94tDShmoMDkbvM'
+  },
+          ios: {
+                          production: false
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
