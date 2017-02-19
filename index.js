0f110bcd3ceba906aef988c4dbf34ea841eede0a
dindar123 <dr.deenmohd@gmail.com>
2017-02-19 11:50:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,6 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Messaging", "_user"] // List of classes to support for query subscriptions
   }
-  push: android: {
-                      senderId: '842306250514',
-                      apikey: 'AIzaSyCl-niHcuWkI0dWgcYEx94tDShmoMDkbvM'
-  },
-          ios: {
-                          production: false
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
