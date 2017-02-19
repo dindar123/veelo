45717dd106a4169450e2beb3a79801be0655fa29
dindar123 <dr.deenmohd@gmail.com>
2017-02-19 11:57:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://veelo.herokuapp.com/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://veelo.herokuapp.com',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Messaging", "_user"] // List of classes to support for query subscriptions
   }
